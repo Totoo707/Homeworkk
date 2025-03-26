39e3df0aafa554d256034245aef54b487d6056d8
Totoo707 <thomaslozancic@gmail.com>
2025-03-26 03:36:55 -0500

    contact popup

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -40,60 +40,7 @@ export default function Page() {
       ></div>
 
       <div className="relative z-10 flex flex-col min-h-screen">
-        {/* HEADER */}
-        <header className="bg-black/50 backdrop-blur-md py-6 px-8">
-          <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center">
-          <Link href="/">
-          <h1 className="text-4xl md:text-5xl font-extrabold tracking-wider text-white cursor-pointer">
-            Next.js App
-          </h1>
-          </Link>
-            <nav className="mt-4 md:mt-0">
-              <ul className="flex space-x-8">
-                <li>
-                  <Link
-                    href="/"
-                    className="text-lg text-white hover:text-yellow-400 transition-colors duration-300"
-                  >
-                    Accueil
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/deuxiemepage"
-                    className="text-lg text-white hover:text-yellow-400 transition-colors duration-300"
-                  >
-                    Articles
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/troisiemepage"
-                    className="text-lg text-white hover:text-yellow-400 transition-colors duration-300"
-                  >
-                    Ajout d'article
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/quatriemepage"
-                    className="text-lg text-white hover:text-yellow-400 transition-colors duration-300"
-                  >
-                    Contact
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/quatriemepage/voirmessage"
-                    className="text-lg text-white hover:text-yellow-400 transition-colors duration-300"
-                  >
-                    Voir Le Contact
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </header>
+
 
         {/* MAIN CONTENT */}
         <main className="flex-grow flex flex-col items-center justify-center px-8 py-16">
@@ -193,34 +140,6 @@ export default function Page() {
          
         </main>
 
-        {/* FOOTER */}
-        <footer className="bg-black/50 backdrop-blur-md py-6 px-8">
-          <div className="max-w-7xl mx-auto text-center">
-            <p className="text-base text-gray-300">
-              © 2025 Next.js. All rights reserved.
-            </p>
-            <div className="flex justify-center space-x-6 mt-4">
-              <Link
-                href="https://twitter.com"
-                className="text-lg text-gray-300 hover:text-pink-300 transition-colors duration-300"
-              >
-                Twitter
-              </Link>
-              <Link
-                href="https://facebook.com"
-                className="text-lg text-gray-300 hover:text-pink-300 transition-colors duration-300"
-              >
-                Facebook
-              </Link>
-              <Link
-                href="https://instagram.com"
-                className="text-lg text-gray-300 hover:text-pink-300 transition-colors duration-300"
-              >
-                Instagram
-              </Link>
-            </div>
-          </div>
-        </footer>
       </div>
     </div>
   );
